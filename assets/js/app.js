6b949e3985ca41332b3c5f23e1910dbd2edd3d4b
grenzk <74033991+grenzk@users.noreply.github.com>
2022-06-12 11:08:07 +0800

    Add p element inside div

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,13 @@ const board = document.getElementById('js-board')
 const createBoard = () => {
   for (let i = 0; i < 9; i++) {
     let square = document.createElement('div')
+    let sign = document.createElement('p')
+
     square.classList.add('square-field')
     square.setAttribute('data-index', i)
+    sign.classList.add('sign-text')
 
+    square.appendChild(sign)
     board.appendChild(square)
   }
 }
