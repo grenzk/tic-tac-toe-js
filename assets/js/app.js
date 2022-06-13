05454c168ca2ea22cf6c66019db391ccda9d3872
grenzk <74033991+grenzk@users.noreply.github.com>
2022-06-13 15:54:25 +0800

    Add win and draw functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,20 @@
 const board = document.getElementById('js-board')
+const winMessage = document.getElementById('js-win-message')
 
 let historyState = []
 let currentState
 let turn = 'X'
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 const boardState = () => {
   let board = [
@@ -15,29 +27,6 @@ const boardState = () => {
 }
 
 currentState = boardState()
-
-const checkWinner = () => {
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-
-  let isWinner = winPatterns.filter(winPattern => {
-    return (
-      currentState[winPattern[0]] &&
-      currentState[winPattern[0]] === currentState[winPattern[1]] &&
-      currentState[winPattern[1]] === currentState[winPattern[2]]
-    )
-  })
-
-  return isWinner.length > 0 ? currentState[isWinner[0][0]] : false
-}
 
 const createBoard = (() => {
   for (let i = 0; i < 9; i++) {
@@ -53,6 +42,28 @@ const createBoard = (() => {
   }
 })()
 
+const squares = document.querySelectorAll('.square-field')
+
+const checkIfWin = () => {
+  return winPatterns.some(winPattern => {
+    return winPattern.every(pos => {
+      return squares[pos].textContent === turn
+    })
+  })
+}
+
+const checkIfDraw = () => {
+  return [...squares].every(square => {
+    return square.textContent.includes('X') || square.textContent.includes('O')
+  })
+}
+
+const disableSquares = () => {
+  squares.forEach(square => {
+    square.setAttribute('disabled', '')
+  })
+}
+
 var renderTurn = function (square) {
   let text = square.querySelector('.sign-text')
 
@@ -67,6 +78,17 @@ var renderTurn = function (square) {
   currentState[selected] = turn
   text.textContent = turn
 
+  if (checkIfWin()) {
+    winMessage.textContent = `Player ${turn} Wins!`
+    disableSquares()
+    return
+  }
+
+  if (checkIfDraw()) {
+    winMessage.textContent = `It's a Draw!`
+    return
+  }
+
   // Update turn
   turn = turn === 'X' ? 'O' : 'X'
 }
@@ -74,7 +96,6 @@ var renderTurn = function (square) {
 document.addEventListener(
   'click',
   function (e) {
-    // If a .game-square was clicked
     if (
       e.target.matches('.square-field') &&
       !e.target.hasAttribute('disabled')
